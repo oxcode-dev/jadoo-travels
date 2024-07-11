77c229a6eebfcaf1fb519b460cf810becdf5fbf6
oxcode <mrexcelsam1@live.com>
2024-07-11 15:06:03 +0100

    Navbar update

--- src/components/Tips.tsx
+++ src/components/Tips.tsx
@@ -1,18 +1,20 @@
 import BookingImage from '../assets/img/booking-img.svg'
+import PaymentIcon from '../assets/img/cab-icon.svg'
+
 export const Tips = () => {
     return (
         <>
             <div className="container mx-auto py-16">
                 <div className="w-full flex flex-wrap">
                     <div className="w-1/2">
-                        <div className='flex flex-col'>
-                            <p>
+                        <div className='flex flex-col space-y-6'>
+                            <p className="uppercase text-slate-500 font-semibold text-base">
                                 Easy and Fast
                             </p>
-                            <h2>
-                                Book Your Next Trip In 3 Easy Steps
+                            <h2 className="text-5xl leading-tight text-[#181E4B] font-semibold">
+                                Book Your Next Trip <br /> In 3 Easy Steps
                             </h2>
-                            <div className='space-y-2'>
+                            <div className='space-y-4 py-4'>
                                 <TipCard />
                                 <TipCard />
                                 <TipCard />
@@ -31,13 +33,15 @@ export const Tips = () => {
 const TipCard = () => {
     return (
         <>
-            <div className='flex flex-nowrap items-center  w-full sm:max-w-md'>
-                <div className='w-12 h-12 bg-red-600 rounded-lg'></div>
+            <div className='flex flex-nowrap items-center space-x-3 w-full sm:max-w-sm'>
+                <div className='w-12 h-12 bg-red-600 rounded-lg'>
+                    <img src={PaymentIcon} className='object-cover w-full h-full' />
+                </div>
                 <div className=''>
-                    <p>
+                    <p className='text-base font-semibold text-slate-600'>
                         Choose Destination
                     </p>
-                    <p>
+                    <p className='text-sm font-normal text-slate-500'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sit.
                     </p>
                 </div>
