7e9ec433650d8fb47224c00fe08bbee25ab2de01
oxcode <mrexcelsam1@live.com>
2024-07-12 09:02:36 +0100

    Navbar update

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -6,7 +6,7 @@ export const Parnters = () => {
                 <div className="w-full py-8 grid grid-cols-2 md:grid-cols-5 gap-6">
                     {
                         partnersList.map((list, key) => (
-                            <div key={key} className='bg-white rounded-2xl hover:shadow-lg flex items-center justify-center grayscale hover:grayscale-0 h-24 py-10'>
+                            <div key={key} title={list.name} className='bg-white rounded-2xl hover:shadow-lg flex items-center justify-center grayscale hover:grayscale-0 h-24 py-10'>
                                 <img src={list.image} alt={list.name} />
                             </div>
                         ))
---
+++ src/components/Subscribe.tsx
@@ -0,0 +1,11 @@
+
+export const Subscribe = () => {
+    return (
+        <>
+            <div className="container mx-auto py-16">
+                <div className="w-full flex flex-wrap">
+                </div>
+            </div>
+        </>
+    )
+}
