f5c63eba4a54e74ad8d5c70ce25d2cd46c8efd85
oxcode <mrexcelsam1@live.com>
2024-07-14 02:25:55 +0100

    Navbar update

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,19 +2,19 @@
 export const Testimonials = () => {
     return (
         <>
-           <div className="container mx-auto py-12">
-                <div className="w-full flex flex-wrap">
-                    <div className="w-1/2">
-                        <div className='flex flex-col space-y-6'>
+           <div className="container mx-auto py-12 px-4">
+                <div className="w-full flex flex-wrap md:flex-nowrap">
+                    <div className="w-full">
+                        <div className='flex flex-col space-y-6 pb-6'>
                             <p className="uppercase text-slate-500 font-semibold text-base">
                                 Testimonials
                             </p>
-                            <h2 className="text-5xl leading-tight text-[#181E4B] font-semibold">
+                            <h2 className="text-3xl md:text-5xl leading-tight text-[#181E4B] font-semibold">
                                 What People Say <br /> About Us.
                             </h2>
                         </div>
                     </div>
-                    <div className="w-1/2 relative">
+                    <div className="w-full relative">
                         <div>
                             <TestimonialCard />
                         </div>
