0564270535bdab1ca14ee53410ed238145fe55bf
oxcode <mrexcelsam1@live.com>
2024-07-14 02:28:00 +0100

    Navbar update

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -6,15 +6,15 @@ export const Subscribe = () => {
     return (
         <>
             <div className="container mx-auto py-12 relative px-4">
-                <img src={SubscribeIcon} className='absolute top-4 left-28 z-10' />
+                <img src={SubscribeIcon} className='absolute top-4 left-0 md:left-28 z-10' />
                 <img src={SubscribeIcon} className='absolute top-4 right-0 z-10' />
                 <img src={SubscribeDecore} className='absolute bottom-0 md:-right-8' />
                 <div className="w-full bg-purple-50 relative rounded-3xl rounded-tl-5xl overflow-hidden relative md:rounded-tl-[10rem]">
                     <img src={SubscribeDown} className='absolute top-0 right-0' />
                     <img src={SubscribeUp} className='absolute bottom-0 -left-24' />
 
-                    <div className="w-full md:max-w-3xl mx-auto py-12 flex flex-col justify-center items-center">
-                        <p className="text-3xl leading-tight text-slate-500 font-semibold text-center leading-relaxed py-8">
+                    <div className="w-full md:max-w-3xl mx-auto py-12 px-4 flex flex-col justify-center items-center">
+                        <p className="text-xl md:text-3xl leading-tight text-slate-500 font-semibold text-center leading-relaxed py-8">
                             Subscribe to get information, latest news other interesting offers about Cobhams.
                         </p>
                         <div className="w-full py-10 relative z-2">
