3e9836ce86c487d6f17dd54f86fe5b9a340a2012
oxcode <mrexcelsam1@live.com>
2024-08-08 01:00:01 +0100

    Navbar update

---
+++ src/data/hero-variants.js
@@ -0,0 +1,89 @@
+export const titleVariants = {
+    hidden: { y: -50, opacity: 0, },
+    show: { 
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            duration: 1.25,
+            delay: 1,
+        }
+    },
+}
+
+export const opacityVariants = {
+    initial: {
+        opacity: 0,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            duration: 0.7,
+            staggerChildren: 0.1,
+        },
+    },
+};
+
+export const leftVariants = {
+    initial: {
+        x: -100,
+        // y: -100,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+export const rightVariants = {
+    initial: {
+        x: 100,
+        opacity: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+};
+
+export const imgVariants = {
+    initial: {
+        height: '0%',
+        opacity: 0,
+    },
+    animate: {
+        height: '100%',
+        opacity: 1,
+        transition: {
+            duration: 2,
+            staggerChildren: 0.1,
+        },
+    },
+};
+
+export const letterVariants = {
+    initial: {
+        // x: 100,
+        height: 0,
+        opacity: 0,
+    },
+    animate: {
+        // x: 0,
+        height: 100,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.1,
+        },
+    },
+};
