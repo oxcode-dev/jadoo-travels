e761da186288ed997e3db6df0b8cc9dad78f8344
oxcode <mrexcelsam1@live.com>
2024-08-08 01:02:16 +0100

    Navbar update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,92 +2,9 @@ import HeroBanner from "../assets/img/hero-bg.svg"
 import HeroImg from "../assets/img/hero.svg"
 import DecoreImg from "../assets/img/Decore.svg"
 import { motion } from 'framer-motion'
+import { opacityVariants, imgVariants, leftVariants, rightVariants} from '../data/hero-variants.js'
 
-const titleVariants = {
-    hidden: { y: -50, opacity: 0, },
-    show: { 
-        y: 0,
-        opacity: 1,
-        transition: {
-            type: "spring",
-            duration: 1.25,
-            delay: 1,
-        }
-    },
-}
-const opacityVariants = {
-    initial: {
-        opacity: 0,
-    },
-    animate: {
-        opacity: 1,
-        transition: {
-            duration: 0.7,
-            staggerChildren: 0.1,
-        },
-    },
-};
 
-const leftVariants = {
-    initial: {
-        x: -100,
-        // y: -100,
-        opacity: 0,
-    },
-    animate: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-            duration: 1,
-            staggerChildren: 0.2,
-        },
-    },
-};
-const rightVariants = {
-    initial: {
-        x: 100,
-        opacity: 0,
-    },
-    animate: {
-        x: 0,
-        opacity: 1,
-        transition: {
-            duration: 1,
-            staggerChildren: 0.1,
-        },
-    },
-};
-const imgVariants = {
-    initial: {
-        height: '0%',
-        opacity: 0,
-    },
-    animate: {
-        height: '100%',
-        opacity: 1,
-        transition: {
-            duration: 2,
-            staggerChildren: 0.1,
-        },
-    },
-};
-const letterVariants = {
-    initial: {
-        // x: 100,
-        height: 0,
-        opacity: 0,
-    },
-    animate: {
-        // x: 0,
-        height: 100,
-        opacity: 1,
-        transition: {
-            duration: 1,
-            staggerChildren: 0.1,
-        },
-    },
-};
 export const Hero = () => {
     return (
         <>
